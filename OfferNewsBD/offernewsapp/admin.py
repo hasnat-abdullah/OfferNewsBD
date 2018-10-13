831a5131876a50f242bda25f64ffe0d42cbaaec9
abdullah <abdullah.2010bd@gmail.com>
2018-10-13 23:27:11 +0600

    Database add in admin panel

--- OfferNewsBD/offernewsapp/admin.py
+++ OfferNewsBD/offernewsapp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Company, Branch, Category, Post, Coupon, Contact, Featured, FeaturePricing, AdPricing, FeaturedPosition, Meta, Advertise, Transaction
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(Branch)
+admin.site.register(Category)
+admin.site.register(Coupon)
+admin.site.register(Post)
+admin.site.register(Contact)
+admin.site.register(Featured)
+admin.site.register(FeaturePricing)
+admin.site.register(AdPricing)
+admin.site.register(FeaturedPosition)
+admin.site.register(Meta)
+admin.site.register(Advertise)
+admin.site.register(Transaction)
