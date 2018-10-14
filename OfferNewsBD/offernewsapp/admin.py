46864e5a5ef790df160c7f00bb8a9cd09ce5a62c
abdullah <abdullah.2010bd@gmail.com>
2018-10-14 21:43:07 +0600

    Model Creation and Admin panel option added 14/10/2018

--- OfferNewsBD/offernewsapp/admin.py
+++ OfferNewsBD/offernewsapp/admin.py
@@ -1,19 +1,125 @@
 from django.contrib import admin
-from .models import User, Company, Branch, Category, Post, Coupon, Contact, Featured, FeaturePricing, AdPricing, FeaturedPosition, Meta, Advertise, Transaction
+from .models import Profile, Company, Branch, Category, Post, Coupon, Contact, Featured, FeaturePricing, AdPricing, FeaturedPosition, Meta, Advertise
 
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Company)
-admin.site.register(Branch)
-admin.site.register(Category)
-admin.site.register(Coupon)
-admin.site.register(Post)
-admin.site.register(Contact)
-admin.site.register(Featured)
-admin.site.register(FeaturePricing)
-admin.site.register(AdPricing)
-admin.site.register(FeaturedPosition)
-admin.site.register(Meta)
-admin.site.register(Advertise)
-admin.site.register(Transaction)
+
+class ProfileModel(admin.ModelAdmin):
+    list_display = ["__str__","phn","email"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Profile
+
+admin.site.register(Profile, ProfileModel)
+
+
+class CompanyModel(admin.ModelAdmin):
+    list_display = ["__str__", "comPhn", "AuthorId"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Company
+
+admin.site.register(Company, CompanyModel)
+
+
+class BranchModel(admin.ModelAdmin):
+    list_display = ["__str__", "comId"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Branch
+admin.site.register(Branch, BranchModel)
+
+
+class CategoryModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Category
+admin.site.register(Category, CategoryModel)
+
+
+class CouponModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Coupon
+admin.site.register(Coupon, CouponModel)
+
+
+class PostModel(admin.ModelAdmin):
+    list_display = ["__str__", "postedOn", "comName", "category", "dealType", "isActive", "expiredOn",]
+    search_fields = ["__str__"]
+    list_filter = ["postedOn", "category"]
+    list_per_page = 30
+
+    class Meta:
+        Model = Post
+admin.site.register(Post, PostModel)
+
+
+class ContactModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Contact
+admin.site.register(Contact, ContactModel)
+
+
+class FeaturedModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Featured
+admin.site.register(Featured, FeaturedModel)
+
+
+class FeaturePricingModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = FeaturePricing
+admin.site.register(FeaturePricing, FeaturePricingModel)
+
+
+class AdPricingModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = AdPricing
+admin.site.register(AdPricing, AdPricingModel)
+
+
+class FeaturedPositionModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__", "details"]
+
+    class Meta:
+        Model = FeaturedPosition
+admin.site.register(FeaturedPosition, FeaturedPositionModel)
+
+
+class MetaModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Meta
+admin.site.register(Meta, MetaModel)
+
+
+class AdvertiseModel(admin.ModelAdmin):
+    list_display = ["__str__"]
+    search_fields = ["__str__"]
+
+    class Meta:
+        Model = Advertise
+admin.site.register(Advertise, AdvertiseModel)
--- OfferNewsBD/offernewsapp/models.py
+++ OfferNewsBD/offernewsapp/models.py
@@ -1,47 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import User
-#
-#Coding By Abdullah on 8/10/2018
 
-class User(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
+
+# Coding By Abdullah on 8/10/2018
+
+class Profile (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     phn = models.CharField(max_length=12, unique=True)
     email = models.EmailField(unique=True)
-    lastLogin = models.DateTimeField()
-    infoUpdatedOn = models.DateField(auto_now=True,auto_now_add=False)
+    infoUpdatedOn = models.DateField(auto_now=True, auto_now_add=False)
     createdOn = models.DateField(auto_now=False, auto_now_add=True)
-    userPic = models.ImageField()
-    pas = models.TextField(max_length=40)
+    userPic = models.ImageField(default='defaultProPic.jpg', upload_to='pro_pics', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
 
 
-class Company(models.Model):
-    comId = models.PositiveIntegerField(primary_key=True)
-    comName = models.CharField(max_length=60,blank=False)
+class Company (models.Model):
+    comName = models.CharField(max_length=60, blank=False)
     comDes = models.TextField()
     comAddress = models.TextField(max_length=200)
     comPhn = models.CharField(max_length=12)
-    comWeb = models.CharField(blank=True)
-    comImage = models.ImageField()
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    comWeb = models.CharField(max_length=50, blank=True)
+    comImage = models.ImageField(default='defaultProPic.jpg', upload_to='com_pics', blank=True)
+    AuthorId = models.ForeignKey(User, on_delete=models.CASCADE)
     infoUpdatedOn = models.DateField(auto_now=True, auto_now_add=False)
     createdOn = models.DateField(auto_now=False, auto_now_add=True)
     isVerified = models.BooleanField(blank=False)
 
-
-class Branch (models.Model):
-    branchId = models.PositiveIntegerField(primary_key=True)
-    managerId=models.ForeignKey(User, on_delete=models.CASCADE)
-    comAddress = models.TextField(max_length=200)
-    comPhn = models.CharField(max_length=12)
-    comId = models.ForeignKey(Company,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comName
 
 
-class Category (models.Model):
-    catId = models.PositiveIntegerField(primary_key=True)
+class Branch(models.Model):
+    branchName = models.CharField(max_length=20)
+    managerId = models.ForeignKey(User, on_delete=models.CASCADE)
+    branchAddress = models.TextField(max_length=200)
+    branchPhn = models.CharField(max_length=12)
+    comId = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.branchName
+
+
+class Category(models.Model):
     catName = models.CharField(max_length=20)
+    catIcon = models.FileField(default='defaultProPic.jpg', upload_to='cat_pics', blank=True)
+
+    def __str__(self):
+        return self.catName
 
 
-class Post (models.Model):
+class Post(models.Model):
     DEAL = 'D'
     COUPON = 'C'
     PERCENTAGE = 'P'
@@ -57,31 +67,122 @@ class Post (models.Model):
         (TAKA, 'Taka'),
         (GIFT, 'Gift'),
     )
-    postId = models.PositiveIntegerField(primary_key=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
-    author = models.CharField(max_length=40)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     comName = models.ForeignKey(Company, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     dealType = models.CharField(max_length=1, choices=Deal_Type, default=DEAL)
     AmountType = models.CharField(max_length=1, choices=Amount_Type, default=PERCENTAGE)
     Amount = models.CharField(max_length=200)
-    postImage = models.ImageField()
-    isActive = models.BooleanField()
+    postImage = models.ImageField(default='default.jpg', upload_to='post_pics', blank=True)
+    isActive = models.BooleanField(default=True)
+    isFeatured = models.BooleanField(default=False)
+    goingUrl = models.URLField()
     postedOn = models.DateTimeField(auto_now=False, auto_now_add=True)
     expiredOn = models.DateTimeField()
     editedOn = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    def __str__(self):
+        return self.title
 
-class Coupon (models.Model):
+
+class Coupon(models.Model):
     postId = models.ForeignKey(Post, on_delete=models.CASCADE)
     couponCode = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.couponCode
 
-class Contact (models.Model):
+
+class Contact(models.Model):
     name = models.CharField(max_length=40)
     email = models.EmailField(max_length=50)
     subject = models.CharField(max_length=100)
     message = models.TextField()
     sendOn = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Featured(models.Model):
+    FEATURE_POST = 'FP'
+    FEATURE_COMPANY = 'FC'
+
+    Featured_Type = (
+        (FEATURE_POST, 'Feature_Post'),
+        (FEATURE_COMPANY, 'Feature_Company'),
+    )
+    featuredType = models.CharField(max_length=2, choices=Featured_Type, default=FEATURE_POST)
+    postId = models.ForeignKey(Post, on_delete=models.CASCADE)
+    featuredExpireDate = models.DateField()
+    featuredDate = models.DateField(auto_now=False, auto_now_add=True)
+    isActive = models.BooleanField(default=True)
+    # price = models.ForeignKey(Pricing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.featuredType
+
+
+class FeaturePricing(models.Model):
+    FEATURE_POST = 'FP'
+    FEATURE_COMPANY = 'FC'
+
+    Featured_Type = (
+        (FEATURE_POST, 'Feature_Post'),
+        (FEATURE_COMPANY, 'Feature_Company'),
+
+    )
+    durationType = models.CharField(max_length=20)
+    featuredType = models.CharField(max_length=2, choices=Featured_Type, default=FEATURE_POST)
+    # position = models.ForeignKey(FeaturedPosition, on_delete=models.CASCADE())
+    priceAmount = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.featuredType
+
+
+class AdPricing(models.Model):
+    durationType = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.durationType
+
+
+class FeaturedPosition(models.Model):
+    COVER = 'C'
+    FIRST_PAGE = 'F'
+    POPUP = 'P'
+
+    Position_Type = (
+        (COVER, 'Cover'),
+        (FIRST_PAGE, 'First_Page'),
+        (POPUP, 'Popup'),
+    )
+    position = models.CharField(max_length=1, choices=Position_Type, default=FIRST_PAGE)
+
+    def __str__(self):
+        return self.position
+
+
+class Meta(models.Model):
+    title = models.CharField(max_length=200)
+    keyword = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+class Advertise(models.Model):
+    advertiser = models.ForeignKey(User, on_delete=models.CASCADE)
+    durationType = models.CharField(max_length=20)
+    adExpireDate = models.DateField()
+    adDate = models.DateField(auto_now=False, auto_now_add=True)
+    isActive = models.BooleanField(default=True)
+    price = models.ForeignKey(AdPricing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.advertiser
